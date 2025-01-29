0e34bf7bbf4541dd24dfde4d83365070c7d72fe3
Tyler Mercer <acct@tylermercer.net>
2025-01-28 20:20:47 -0700

    Uninstall remark-footnote
    Not needed; I just use GFM style footnotes now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import cloudflare from "@astrojs/cloudflare";
 import { defineConfig } from 'astro/config';
-import remarkFootnote from 'remark-footnotes';
 import remarkEmdash from './lib/plugins/remark/emdash.js';
 import rawFonts from './lib/plugins/vite/rawFonts.js';
 
@@ -10,11 +9,8 @@ export default defineConfig({
   adapter: cloudflare(),
   markdown: {
     remarkPlugins: [
-      // @ts-ignore -- doesn't like remarkFootnote
-      [remarkFootnote, {
-        inlineNotes: true
-      }],
-      remarkEmdash],
+      remarkEmdash
+    ],
   },
   vite: {
     plugins: [rawFonts(['.woff'])],
